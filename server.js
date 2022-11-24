346065645cbb051a4d3599a111a1703377db24ce
MUthuhari <muthuharinisansala2@gmail.com>
2022-11-24 22:50:52 +0530

    new modified

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
+
+//import routes
+const postRoutes = require('./routes/posts');
+//app middleware
+app.use(bodyParser.json());
+
+app.use(postRoutes);
+
+
 const PORT = 8000;
 const DB_URL = 'mongodb+srv://Muthu:Muthu123@mernapp.1omssvd.mongodb.net/Muthu?retryWrites=true&w=majority';
+
 
 mongoose.connect(DB_URL,)
 .then(() =>{
